4180a1642cc049b0593d639124f5515a7e5e8cd4
Benjamin Chrétien <chretien@lirmm.fr>
2013-06-20 13:00:11 +0900

    　Do minor modifications.

--- test/check_bug.cu
+++ test/check_bug.cu
@@ -1,14 +1,13 @@
-// See: http://choorucode.com/2011/03/page/2/
-
 #include <cuda.h>
 
-__global__ void fooKernel( const int* inArr, int num, int* outArr )
+__global__ void bug_kernel(const int* in, int* out, int num)
 {
-    const int threadNum     = gridDim.x * blockDim.x;
-    const int curThreadIdx  = ( blockIdx.x * blockDim.x ) + threadIdx.x;
+    const int threadNum = gridDim.x * blockDim.x;
+    const int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for ( int i = curThreadIdx; i < num; i += threadNum )
-        outArr[ i ] = inArr[ i ] + inArr[ i + 1 ];
+    for (int i = tid; i < num; i += threadNum)
+        // Out of bounds access here
+        out[i] = in[i] + in[i + 1];
 
     return;
 }
@@ -17,22 +16,22 @@ int main()
 {
     // Allocate memory
     const int num   = 100;
-    int* dInArr     = NULL;
-    int* dOutArr    = NULL;
-    const int size  = num * sizeof( *dInArr );
+    int* d_in       = NULL;
+    int* d_out      = NULL;
+    const int size  = num * sizeof(*d_in);
 
-    cudaMalloc( &dInArr, size );
-    cudaMalloc( &dOutArr, size );
+    cudaMalloc(&d_in, size);
+    cudaMalloc(&d_out, size);
 
     // Compute
     const int blocksPerGrid     = 128;
     const int threadsPerBlock   = 128;
 
-    fooKernel<<< blocksPerGrid, threadsPerBlock >>>( dInArr, num, dOutArr );
+    bug_kernel<<< blocksPerGrid, threadsPerBlock >>>(d_in, d_out, num);
 
     // Free memory
-    cudaFree( dInArr );
-    cudaFree( dOutArr );
+    cudaFree(d_in);
+    cudaFree(d_out);
 
     return 0;
 }
